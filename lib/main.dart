5dd7ade210664697c6284a25448f4b196684bebc
johnyworld <johnyworld@naver.com>
2021-02-19 08:40:17 +0900

    Done with Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,25 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(title: Text(title)),
+        body: Center(child: Text('My Page')),
+        drawer: Drawer(
+            child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+          DrawerHeader(
+            child: Text('Drawer Header'),
+            decoration: BoxDecoration(color: Colors.blue),
+          ),
+          ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              }),
+          ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              }),
+        ])));
   }
 }
